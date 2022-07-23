fe1f1f1dece30e5e4ec5349292ec18ec8d9a893a
fry_guest <fryguestcc@gmail.com>
2022-07-24 01:12:15 +0800

    Update

---
+++ Maplestory-Maid/Job.h
@@ -0,0 +1,77 @@
+#ifndef JOB_H
+#define JOB_H
+
+enum Job
+{
+    // 冒险家
+    Hero, //英雄
+    Paladin, //圣骑士
+    DarkKnight, //黑骑士
+    Bishop, //主教
+    ArchMage_Fire_Poison, //大魔导师(火,毒)
+    ArchMage_Ice_Lightning, //大魔导师(冰,雷)
+    BowMaster, //箭神
+    Marksman, //神射手
+    PathFinder, //开拓者
+    Shadower, //暗影神偷 (刀飞)
+    NightLord, //夜使者 (标飞)
+    BladeMaster, //影武者 (双刀)
+    Buccaneer, //拳霸
+    Corsair, //枪神
+    CannonMaster,//重炮指挥官
+
+    //六英雄
+    Aran, //狂狼勇士
+    Evan, //龙魔导士
+    Mercedes, //精灵游侠
+    Phantom, //幻影侠盗
+    Luminous, //夜光
+    Shade, // 隐月
+
+    //西格诺斯骑士团
+    Mihile, //米哈逸
+    DawnWarrior, //圣魂剑士
+    BlazeWizard, //烈焰巫师
+    WindArcher, //破风使者
+    NightWalker, //暗夜行者
+    ThunderBreaker, //闪雷悍将
+
+    //末日反抗军
+    DemonSlayer, //恶魔杀手
+    DemonAvenger, //恶魔复仇者
+    Blaster, //爆拳枪神
+    BattleMage, //炼狱巫师
+    WildHunter, //狂豹猎人
+    Mechanic, //机甲战神
+    Xenon, //杰诺
+
+    //超新星
+    Kaiser, //凯撒
+    Cadena, //卡蒂娜
+    AngelicBuster, //天使破坏者
+    Kain, //凯殷
+
+    //雷普
+    Adele, //阿黛尔
+    Ark, //亚克
+    Illium, //伊利恩
+
+    //阿尼玛
+    Hoyoung, //虎影
+    Lara, //菈菈
+
+    //其他
+    Zero, //神之子
+    Kinesis, //凯内西斯
+
+    //海外职业
+    Kanna, //阴阳师
+    Hayato, //剑豪
+    BeastTamer, //幻兽师
+    Jett, //杰特
+    MoXuan //墨玄
+};
+
+
+
+#endif // JOB_H
